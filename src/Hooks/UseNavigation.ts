d4791f64cf10aa21f21bae7147aa8df0fbd49a27
Erik Slovak <erik.slovak@technologystudio.sk>
2023-05-26 10:23:03 +0200

    fix: remove `getParent` and use only `getRoot`

--- src/Hooks/UseNavigation.ts
+++ src/Hooks/UseNavigation.ts
@@ -5,6 +5,7 @@
 **/
 
 import {
+  useCallback,
   useMemo,
 } from 'react'
 import type { NavigationProp } from '@react-navigation/native'
@@ -23,14 +24,13 @@ type ParamsMapExtend = Record<string, Record<string, unknown> | undefined>
 export type Navigation<
 NAVIGATION_PROP extends NavigationPropExtend,
 PARAMS_MAP extends ParamsMapExtend,
-> = Omit<NAVIGATION_PROP, 'navigate' | 'getParent'> & {
+> = Omit<NAVIGATION_PROP, 'navigate'> & {
   cancelFlow: () => void,
   finishFlowAndContinue: () => void,
   navigate: <ROUTE_NAME extends keyof PARAMS_MAP>(payload: NavigatePayload<PARAMS_MAP, ROUTE_NAME>) => void,
   requireConditions: (conditionList: Condition[]) => void,
   validateConditions: () => void,
   goBack: (payload?: BackPayload) => void,
-  getParent: (id?: string | undefined) => Navigation<NAVIGATION_PROP, PARAMS_MAP> | undefined,
   getRoot: () => Navigation<NAVIGATION_PROP, PARAMS_MAP>,
 }
 
@@ -77,62 +77,6 @@ const navigationActionsFactory = <
     },
   })
 
-type GetParentFn<
-  NAVIGATION_PROP extends NavigationPropExtend,
-  PARAMS_MAP extends ParamsMapExtend
-> = (id?: string | undefined) => Navigation<NAVIGATION_PROP, PARAMS_MAP> | undefined
-
-const getParentFactory = <
-  NAVIGATION_PROP extends NavigationPropExtend,
-  PARAMS_MAP extends ParamsMapExtend
->(navigation: NAVIGATION_PROP): GetParentFn<NAVIGATION_PROP, PARAMS_MAP> => {
-  const _getParent: GetParentFn<NAVIGATION_PROP, PARAMS_MAP> = (id?: string | undefined) => {
-    const parent = navigation.getParent<NAVIGATION_PROP>(id)
-    if (parent == null) {
-      return undefined
-    }
-    const navigationActions = navigationActionsFactory(parent)
-
-    return {
-      ...parent,
-      ...navigationActions,
-      getParent: _getParent,
-      getRoot: getRootFactory(parent),
-    }
-  }
-  return _getParent
-}
-
-type GetRootFn<
-  NAVIGATION_PROP extends NavigationPropExtend,
-  PARAMS_MAP extends ParamsMapExtend
-> = () => Navigation<NAVIGATION_PROP, PARAMS_MAP>
-const getRootFactory = <
-  NAVIGATION_PROP extends NavigationPropExtend,
-  PARAMS_MAP extends ParamsMapExtend
->(
-    navigation: NAVIGATION_PROP,
-  ): GetRootFn<NAVIGATION_PROP, PARAMS_MAP> => {
-  const getRoot: GetRootFn<NAVIGATION_PROP, PARAMS_MAP> = () => {
-    let parent = navigation.getParent<NAVIGATION_PROP>()
-    while (parent != null) {
-      const nextParent = parent.getParent<NAVIGATION_PROP>()
-      if (nextParent == null) {
-        break
-      }
-      parent = nextParent
-    }
-
-    return {
-      ...parent ?? navigation,
-      ...navigationActionsFactory(parent ?? navigation),
-      getParent: getParentFactory(parent ?? navigation),
-      getRoot,
-    }
-  }
-  return getRoot
-}
-
 export const useNavigation = <
   NAVIGATION_PROP extends NavigationPropExtend,
   PARAMS_MAP extends ParamsMapExtend,
@@ -147,6 +91,23 @@ export const useNavigation = <
     goBack,
   } = useMemo(() => navigationActionsFactory(_navigation), [_navigation])
 
+  const getRoot = useCallback(() => {
+    let parent = _navigation.getParent<NAVIGATION_PROP>()
+    while (parent != null) {
+      const nextParent = parent.getParent<NAVIGATION_PROP>()
+      if (nextParent == null) {
+        break
+      }
+      parent = nextParent
+    }
+
+    return {
+      ...parent ?? _navigation,
+      ...navigationActionsFactory(parent ?? _navigation),
+      getRoot,
+    }
+  }, [_navigation])
+
   const navigation: Navigation<NAVIGATION_PROP, PARAMS_MAP> = useMemo(() => ({
     ..._navigation,
     cancelFlow,
@@ -155,8 +116,7 @@ export const useNavigation = <
     validateConditions,
     navigate,
     goBack,
-    getParent: getParentFactory<NAVIGATION_PROP, PARAMS_MAP>(_navigation),
-    getRoot: getRootFactory<NAVIGATION_PROP, PARAMS_MAP>(_navigation),
-  }), [_navigation, cancelFlow, finishFlowAndContinue, goBack, navigate, requireConditions, validateConditions])
+    getRoot,
+  }), [_navigation, cancelFlow, finishFlowAndContinue, getRoot, goBack, navigate, requireConditions, validateConditions])
   return navigation
 }
