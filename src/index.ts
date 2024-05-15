f6f6eea88bb49c7b3476d0f2708ebe0e3c8cfc32
Erik Slovak <erik.slovak@technologystudio.sk>
2024-05-15 14:27:25 +0200

    chore: remove unnecessary comments

--- src/index.ts
+++ src/index.ts
@@ -17,40 +17,6 @@ export {
 } from './Containers/InjectedNavigationContainer'
 export { ROOT_NAVIGATOR_ID } from './Model'
 export * from './Model/Types'
-// import { navigationManager } from './Api/NavigationManager'
-// import { isInitialNavigationOptions } from './Api/NavigationOptionsHelper'
-// import type { NavigationProps } from './Screens/Types'
-// import {
-//   navigationParams,
-// } from './Screens'
-// import {
-//   type NavigatePayload,
-//   type NavigationAction,
-//   type NavigationAllAction,
-//   type NavigationBackAction,
-//   type NavigationCancelFlowAction,
-//   type NavigationFinishFlowAndContinueAction,
-//   type NavigationNavigateAction,
-//   type NavigationRequireConditionsAction,
-//   type NavigationSetParamsAction,
-//   type NavigationValidateConditionsAction,
-//   types as navigationTypes,
-// } from './Redux/Types/NavigationReduxTypes'
-// import { creators as navigationActionCreators } from './Redux/NavigationRedux'
-// import type {
-//   Condition,
-//   NavigatorType,
-// } from './Model/Types'
-// import { navigatorTypes } from './Model/Types'
-// import {
-//   combineReducers,
-//   createNavigationMiddleware,
-//   createNavigationReducer,
-// } from './Redux'
-// import type {
-//   RootStateFragment,
-//   State as NavigationState,
-// } from './Redux/Types'
 export { ConditionalActions } from './Navigation/ConditionalActions'
 export { onActionFactory } from './Navigation/OnActionFactory'
 export { backHandlerManager } from './Api/BackHandlerManager'
@@ -69,37 +35,3 @@ export {
   useNavigation,
 } from './Hooks/UseNavigation'
 export { navigationParams } from './Screens'
-
-// export {
-//   combineReducers,
-//   conditionalNavigationManager,
-//   configManager,
-//   createNavigationMiddleware,
-//   createNavigationReducer,
-//   isInitialNavigationOptions,
-//   navigationActionCreators,
-//   navigationManager,
-//   navigationParams,
-//   navigationTypes,
-//   navigatorTypes,
-//   registerResolveCondition,
-// }
-
-// export type {
-//   Condition,
-//   NavigatePayload,
-//   NavigationAction,
-//   NavigationAllAction,
-//   NavigationBackAction,
-//   NavigationCancelFlowAction,
-//   NavigationFinishFlowAndContinueAction,
-//   NavigationNavigateAction,
-//   NavigationProps,
-//   NavigationRequireConditionsAction,
-//   NavigationSetParamsAction,
-//   NavigationState,
-//   NavigationValidateConditionsAction,
-//   NavigatorType,
-//   ResolveCondition,
-//   RootStateFragment,
-// }
